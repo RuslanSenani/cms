0eda6c8ed769fa312cd71f65864fd1734882a254
Ruslan <abbasovruslan214@gmail.com>
2021-09-04 18:38:30 +0400

    View qurulusu tek fayl icerisine elave edildi,ViewFolder qurulusu elave olundu

---
+++ panel/application/controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+	private $viewFolder = "";
+	public function __construct()
+	{
+		parent::__construct();
+		$this->viewFolder = "dashboard_v";
+	}
+	public function index()
+	{
+		$this->load->view("{$this->viewFolder}/index");
+	}
+}
