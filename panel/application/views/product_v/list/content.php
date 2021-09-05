95a93fa096b82c2e3e07e4fa9466b9554aeb7e4e
Ruslan <abbasovruslan214@gmail.com>
2021-09-05 13:37:47 +0400

    Product modulunun product listi ucun html sablonu yaradildi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,46 @@
-Product List Content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Product List</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget">
+            <header class="widget-header">
+            </header>
+
+
+
+            <hr class="widget-separator">
+            <div class="widget-body">
+                <div class="table-responsive">
+                    <table id="default-datatable" data-plugin="DataTable" class="table" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>#id</th>
+                                <th>Url</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Status</th>
+                                <th>Progress</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-danger btn-outline"> <i class="fa fa-trash"></i> Delete</a>
+                                    <a href="#" class="btn btn-sm btn-success btn-outline"> <i class="fa fa-pencil-square-o"></i> Update</a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
