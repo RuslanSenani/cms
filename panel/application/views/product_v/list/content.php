d686cbbe6d4919d9646d036e71b891ccf7b4b2e8
Ruslan <abbasovruslan214@gmail.com>
2021-09-05 15:00:12 +0400

    Toggle button elave edildi,Add New Botton  elave olundu, Qeyd Yoxdur paneli elave olundu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,18 +1,23 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Product List</h4>
+        <h4 class="m-b-lg">
+            Product List
+            <a href="#" class="btn btn-outline btn-primary btn-xs pull-right"> <i class="fa fa-plus"></i> Add New </a>
+        </h4>
     </div>
     <div class="col-md-12">
         <div class="widget">
             <header class="widget-header">
             </header>
-
-
-
             <hr class="widget-separator">
             <div class="widget-body">
                 <div class="table-responsive">
-                    <table id="default-datatable" data-plugin="DataTable" class="table" cellspacing="0" width="100%">
+
+                    <div class="alert alert-info text-center">
+                        <p>There is no data available here. Click please <a href="#">Add</a></p>
+                    </div>
+
+                    <table id="default-datatable" data-plugin="DataTable" class="table table-hover" cellspacing="0" width="100%">
                         <thead>
                             <tr>
                                 <th>#id</th>
@@ -30,7 +35,11 @@
                                 <td>System Architect</td>
                                 <td>Edinburgh</td>
                                 <td>61</td>
-                                <td>2011/04/25</td>
+                                <td>
+
+                                    <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+
+                                </td>
                                 <td>
                                     <a href="#" class="btn btn-sm btn-danger btn-outline"> <i class="fa fa-trash"></i> Delete</a>
                                     <a href="#" class="btn btn-sm btn-success btn-outline"> <i class="fa fa-pencil-square-o"></i> Update</a>
