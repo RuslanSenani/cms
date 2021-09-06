fc2baeb13d811c73cc773427edb1dcb1ed906e91
Ruslan <abbasovruslan214@gmail.com>
2021-09-06 16:16:53 +0400

    Sweetalert ile Confirm istifade ederek Qeydleri Silmek gerceklesdirildi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -42,7 +42,9 @@
                                             <input id="" type="checkbox" data-switchery data-color="#10c469" <?= ($item->isActive) ? "checked" : ""; ?> />
                                         </td>
                                         <td>
-                                            <a href="<?= base_url("product/delete/$item->id"); ?>" class="btn btn-sm btn-danger btn-outline"> <i class="fa fa-trash"></i> Delete</a>
+                                            <button data-url="<?= base_url("product/delete/$item->id"); ?>" class="btn btn-sm btn-danger btn-outline remove-btn">
+                                                <i class="fa fa-trash"></i> Delete
+                                            </button>
                                             <a href="<?= base_url("product/update_form/$item->id"); ?>" class="btn btn-sm btn-success btn-outline"> <i class="fa fa-pencil-square-o"></i> Update</a>
                                         </td>
                                     </tr>
--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
-    
+    $(".remove-btn").click(function (e) {
+        var $data_url = $(this).data("url");
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Delete it!',
+            cancelButtonText: "Cancle"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = $data_url;
+            }
+        })
+    })
 
 })
