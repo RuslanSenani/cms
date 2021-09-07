f4f4f72707659529c48019e28c2c3483c6b61763
Ruslan <abbasovruslan214@gmail.com>
2021-09-07 17:12:15 +0400

    Papka yerleri deyistirilen fayillar

--- panel/application/views/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap p-t-0">
-    <footer class="app-footer">
-        <div class="clearfix">
-            <div class="copyright pull-left">Copyright Ruslan | <?= date("Y"); ?> &copy;</div>
-        </div>
-    </footer>
-</div>
