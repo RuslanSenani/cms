de14993607729429163d23d1ccc91fc79e775968
Ruslan <abbasovruslan214@gmail.com>
2021-09-07 13:57:10 +0400

    Upload proseduru tamamlandi

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -164,6 +164,24 @@ class Product extends CI_Controller
         $viewData = new stdClass();
         $viewData->viewFolder = $this->viewFolder;
         $viewData->subViewFolder = "image";
+        $viewData->item = $this->product_model->get(array(
+            "id" => $id
+        ));
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
+    public function image_upload()
+    {
+        $config = array(
+            "allowed_types" => "jpg|jpeg|pnh",
+            "upload_path" => "uploads/$this->viewFolder/"
+        );
+        $this->load->library("upload", $config);
+        $upload = $this->upload->do_upload("file");
+        if ($upload) {
+            echo "Success";
+        } else {
+            echo "Faild";
+        }
     }
 }
--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,10 +1,10 @@
 <div class="row">
-    
+
     <div class="col-md-12">
         <div class="widget">
             <hr class="widget-separator">
             <div class="widget-body">
-                <form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
+                <form action="<?= base_url("product/image_upload"); ?>" class="dropzone" data-plugin="dropzone" data-options="{ url: '<?= base_url("product/image_upload"); ?>'}">
                     <div class="dz-message">
                         <h3 class="m-h-lg">Drop files here or click to upload.</h3>
                         <p class="m-b-lg text-muted">(This is just a demo dropzone. Selected files are not actually uploaded.)</p>
@@ -17,7 +17,7 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Pictures
+            Pictures of the <b><?= $item->title; ?> </b>
         </h4>
     </div>
     <div class="col-md-12">
